3bd481342ce0140ebd96dbdd9dd81594272658ff
abhi <abhijeetvc7@gmail.com>
2020-06-07 22:07:12 +0530

    Multiple components

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Header(){
+   return (
+       <header>This is Header Section</header>
+   )
+}
+
+export default Header
---
+++ src/components/MainContent.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function MainContent(){
+    return (
+        <main>This is the Main content</main>
+    )
+}
+
+export default MainContent
