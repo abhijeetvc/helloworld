0576e266fc1fa5e8c77c14911f1486a7b9e9d370
abhi <abhijeetvc7@gmail.com>
2020-06-08 15:31:42 +0530

    Multiple components and styling in React

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react"
 
 function Header(){
    return (
-       <header>This is Header Section</header>
+       <header className="navbar">This is Header Section</header>
    )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from "./App"
+//import App from "./App"
 
+// function MyApp1(){
+//     const firstName="John"
+//     const lastName="Doe"
 
-ReactDOM.render(<App />,
+//     return(
+//         <h1>Hello {firstName+" "+lastName}</h1>
+//       //  <h1>Hello {'${firstName} ${lastName}'}!</h1>
+//     )
+// }
+
+// function MyApp2(){
+//     const date=new Date()
+
+//     return(
+//         <h1>It is about {date.getHours()%12} o' clock</h1>
+//     )
+// }
+
+function MyApp3(){
+    const date=new Date()
+    const hours=date.getHours()
+    let timeOfDay
+
+   // const styles={color: "#FF8C00", backgroundColor:"#FF2D00", fontSize:"20px"}
+    const styles={ fontSize: 60}
+
+    if(hours<12){
+        timeOfDay="Its Morning"
+        styles.color= "#04756F"
+    }else if(hours>=12 && hours<17){
+        timeOfDay="Its Afternoon"
+        styles.color= "#04756F"
+    }else{
+        timeOfDay="Its evening"
+        styles.color= "#04756F"
+    }
+
+    return(
+        <h1 style={styles}>Hey!!! {timeOfDay}</h1>
+    )
+}
+ReactDOM.render(<MyApp3 />,
 document.getElementById("root"));
